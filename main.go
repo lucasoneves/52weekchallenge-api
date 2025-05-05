f0e194a4528d81d666acc5125fff95f5e79af27d
lucasoneves <casluhc@gmail.com>
2025-05-05 19:40:23 -0300

    Novos recursos

--- main.go
+++ main.go
@@ -6,16 +6,29 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var challenges = []models.Challenge{
+	{ID: "1", Name: "Férias de verão", Description: "Férias de verão com destino ao nordeste", TargetValue: 10000, Progress: 50},
+	{ID: "2", Name: "Playstation 5", Description: "Console novo 2025", TargetValue: 5000, Progress: 20},
+	{ID: "3", Name: "Playstation 5", Description: "Console novo 2025", TargetValue: 5000, Progress: 45},
+}
+
 func main() {
-	r := gin.Default()
-	r.GET("/challenges", getChallenges)
-	r.Run()
+	router := gin.Default()
+	router.GET("/challenges", getChallenges)
+	router.POST("/challenges", createChallenge)
+	router.Run()
 }
 func getChallenges(c *gin.Context) {
-	var challenges = []models.Challenge{
-		{ID: "1", Name: "Férias de verão", Description: "Férias de verão com destino ao nordeste", TargetValue: 10000, Progress: 50},
-		{ID: "2", Name: "Playstation 5", Description: "Console novo 2025", TargetValue: 5000, Progress: 20},
-		{ID: "3", Name: "Playstation 5", Description: "Console novo 2025", TargetValue: 5000, Progress: 45},
-	}
+
 	c.JSON(200, challenges)
+}
+
+func createChallenge(c *gin.Context) {
+	var newChallenge models.Challenge
+	if err := c.ShouldBindJSON(&newChallenge); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	challenges = append(challenges, newChallenge)
+	c.JSON(200, newChallenge)
 }
