c8287a9d9581296f182f6e06d19ae9cdb5e7dcb7
lucasoneves <casluhc@gmail.com>
2025-05-14 21:19:03 -0300

    Handlers

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"52weeks/internal/data"
+	"52weeks/internal/handler"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,11 +11,11 @@ func main() {
 	data.LoadChallenges()
 
 	router := gin.Default()
-	router.GET("/challenges", getChallenges)
-	router.GET("/challenges/:id", getChallengeByID)
-	router.PUT("/challenges/:id", updateChallenge)
-	router.DELETE("/challenges/:id", deleteChallenge)
-	router.POST("/challenges", createChallenge)
+	router.GET("/challenges", handler.GetChallenges)
+	router.GET("/challenges/:id", handler.GetChallengeByID)
+	router.PUT("/challenges/:id", handler.UpdateChallenge)
+	router.DELETE("/challenges/:id", handler.DeleteChallenge)
+	router.POST("/challenges", handler.CreateChallenge)
 	router.GET("/welcome", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "Olá Mundo"})
 	})
